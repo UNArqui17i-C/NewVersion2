2ac8b69a2aa94bc253f68570ee9a077a3776133a
jaavargasar <jaavargasar@gmail.com>
2017-04-27 23:27:59 -0500

    View my places

---
+++ src/app/layout/tables/places.ts
@@ -0,0 +1,11 @@
+export class Places{
+    email: string;
+    latitude : string;
+    length: string;
+
+    constructor( email:string, latitude: string, length: string ){
+        this.email = email;
+        this.latitude = latitude ;
+        this.length = length;
+    }
+}
--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Places} from "./places";
+import {RequestService} from "../../request.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-tables',
@@ -6,6 +9,45 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./tables.component.scss']
 })
 export class TablesComponent implements OnInit {
-    constructor() { }
-    ngOnInit() { }
+    constructor(private requestService: RequestService, private router: Router) {
+        this.requestService;
+    }
+
+    places: Places[];
+    rightPlaces: Places[] = new Array();
+
+
+    showPlaces(){
+        this.requestService.getPlaces(
+            'http://localhost:5000/places'
+        )
+            .subscribe(
+                places=>{
+                    this.places = places;
+
+                    // console.log('imprimiendo places=' + JSON.stringify(this.placesMapi ));
+
+                    for (let entry of this.places) {
+                        
+                        if(entry.email==localStorage.getItem("email") ){
+                            var newPlace:Places = new Places(
+                                localStorage.getItem("email"),
+                                entry["latitude"],
+                                entry["length"]
+                            )
+                            //console.log(  newPlace);
+                            this.rightPlaces.push(newPlace);
+                        }
+
+                    }
+
+                },
+                err => { console.log(err);}
+            )
+    }
+
+
+    ngOnInit() {
+        this.showPlaces();
+    }
 }
